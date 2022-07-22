2ecf15a0884b96197980fef74c60c90af17b9e3a
harshitgarg3103 <sdpsharshitgarg10b@gmail.com>
2022-07-22 11:48:41 +0530

    Init

---
+++ JPMC-CodeBase/C#/HandsOn/PropertiesAssignment/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PropertiesAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of books: ");
+            int n = int.Parse(Console.ReadLine());
+            Book[] b = new Book[n];
+            for(int i=0;i<n;i++)
+            {
+                b[i] = new Book();
+                Console.WriteLine("ENTER ISBN: ");
+                b[i].ISBN = int.Parse(Console.ReadLine()) ;
+                Console.WriteLine("ENTER quantity: ");
+                b[i].quantit = int.Parse(Console.ReadLine());
+                Console.WriteLine("ENTER BOOKNAME: ");
+                b[i].name = Console.ReadLine();
+                Console.WriteLine("ENTER TITLE: ");
+                b[i].TITLE = Console.ReadLine();
+                Console.WriteLine("ENTER AUTHOR: ");
+                b[i].AUTHOR = Console.ReadLine();
+                Console.WriteLine("ENTER PRICE: ");
+                b[i].PRICE = double.Parse(Console.ReadLine());
+
+            }
+            for (int i = 0; i < n; i++)
+                b[i].display();
+        }
+    }
+}
---
+++ JPMC-CodeBase/C#/HandsOn/PropertiesAssignment/q1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertiesAssignment
+{
+    internal class q1
+    {
+    }
+    class Book
+    {
+        private int isbn,quantity;
+        private string bookname;
+        private string title,author;
+        private double price;
+        public int ISBN
+        {
+            get{ return isbn; }
+            set{ 
+                isbn = value;
+            }
+        }
+        public int quantit
+        {
+            get { return quantity; }
+            set
+            {
+                quantity = value;
+            }
+        }
+        public string name
+        {
+            get { return bookname; }
+            set
+            {
+                bookname = value;
+            }
+        }
+        public string TITLE
+        {
+            get { return title; }
+            set
+            {
+                title = value;
+            }
+        }
+        public string AUTHOR
+        {
+            get { return author; }
+            set
+            {
+                author = value;
+            }
+        }
+        public double PRICE
+        {
+            get { return price; }
+            set
+            {
+                price = value;
+            }
+        }
+        public void display()
+        {
+            double amount = (double)quantity * price;
+            Console.WriteLine("Amount is " + amount);
+        }
+    }
+}
+
